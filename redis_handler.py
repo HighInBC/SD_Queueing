14ce9e5475e8a16728213d79ce1b3fb4a318ba6c
HighInBC <HighInBC@gmail.com>
2023-04-14 10:25:28 -0700

    priority system

--- redis_handler.py
+++ redis_handler.py
@@ -1,4 +1,3 @@
-# redis_handler.py
 import redis
 import json
 
@@ -14,21 +13,22 @@ def connect_to_redis(config):
     )
     return redis_connection
 
-def read_from_ingress_queue(redis_connection, ingress_queue):
+def read_from_ingress_queue(redis_connection, base_queue):
     """
-    Read a job from the ingress queue using the Redis connection object.
+    Read a job from the priority ingress queues using the Redis connection object.
     Return the job request and the client-specific return queue.
     """
-    job = redis_connection.blpop(ingress_queue, timeout=0)
-    if job is not None:
-        # Parse the job as a JSON object
-        job = json.loads(job[1])
-        request = job.get("request")
-        return_queue = job.get("return_queue")
-        return request, return_queue
-    else:
-        # If no job is found, return None
-        return None, None
+    # Prioritize higher priority queues by iterating in decreasing order of priority
+    for i in range(5, -1, -1):
+        current_queue = f"{base_queue}_priority_{i}"
+        job = redis_connection.blpop(current_queue, timeout=0)
+        if job is not None:
+            job = json.loads(job[1])
+            request = job.get("request")
+            return_queue = job.get("return_queue")
+            return request, return_queue
+
+    return None, None
 
 import json
 
@@ -41,8 +41,5 @@ def send_response_to_return_queue(redis_connection, return_queue, original_reque
         "request": original_request,
         "response": response
     }
-    # Convert the response object to a JSON string
     response_str = json.dumps(response_obj)
-
-    # Send the response to the client-specific return queue
     redis_connection.rpush(return_queue, response_str)
