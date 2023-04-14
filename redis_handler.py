12db8b5d8057578cc1bb07341248c77d1296a85e
HighInBC <HighInBC@gmail.com>
2023-04-14 10:17:06 -0700

    untested

--- redis_handler.py
+++ redis_handler.py
@@ -1,6 +1,6 @@
 # redis_handler.py
-
 import redis
+import json
 
 def connect_to_redis(config):
     """
@@ -9,7 +9,7 @@ def connect_to_redis(config):
     redis_connection = redis.StrictRedis(
         host=config["redis_host"],
         port=config["redis_port"],
-        password=config["redis_password"],
+        password=config.get("redis_password", None),
         decode_responses=True
     )
     return redis_connection
@@ -19,11 +19,30 @@ def read_from_ingress_queue(redis_connection, ingress_queue):
     Read a job from the ingress queue using the Redis connection object.
     Return the job request and the client-specific return queue.
     """
-    pass
+    job = redis_connection.blpop(ingress_queue, timeout=0)
+    if job is not None:
+        # Parse the job as a JSON object
+        job = json.loads(job[1])
+        request = job.get("request")
+        return_queue = job.get("return_queue")
+        return request, return_queue
+    else:
+        # If no job is found, return None
+        return None, None
+
+import json
 
 def send_response_to_return_queue(redis_connection, return_queue, original_request, response):
     """
     Package the original request and response (array of base64 images) in a JSON object.
     Send the JSON object to the client-specific return queue using the Redis connection object.
     """
-    pass
+    response_obj = {
+        "request": original_request,
+        "response": response
+    }
+    # Convert the response object to a JSON string
+    response_str = json.dumps(response_obj)
+
+    # Send the response to the client-specific return queue
+    redis_connection.rpush(return_queue, response_str)
