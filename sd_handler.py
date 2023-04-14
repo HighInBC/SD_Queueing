e43ccf5914daf3de9685b33210f0dc7ee7f21a50
HighInBC <HighInBC@gmail.com>
2023-04-14 10:17:24 -0700

    untested

--- sd_handler.py
+++ sd_handler.py
@@ -1,7 +1,20 @@
-# sd_handler.py
+import requests
+import base64
+import json
+import random
 
 def process_stable_diffusion_request(request):
     """
-    Process the Stable Diffusion API request and return an array of base64 images.
+    Process the Stable Diffusion API request and return an array of base64-encoded images.
     """
-    pass
+    sd_request = request.get("request")
+    sd_request['seed'] = random.randint(0, 2**32-1)
+    response = requests.post("127.0.0.1:7860", json=sd_request)
+    images = response.json()['images']
+
+    png_images = []
+    for image in images:
+        img_bytes = base64.b64decode(image)
+        png_images.append(img_bytes)
+
+    return png_images
