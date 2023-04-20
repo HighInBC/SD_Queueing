3f4709c7e0788a06498e285989a27d960fd1ac67
HighInBC <HighInBC@gmail.com>
2023-04-19 22:09:15 -0700

    implement priority

--- bulk_generate_remote.py
+++ bulk_generate_remote.py
@@ -13,14 +13,15 @@ from sshtunnel import SSHTunnelForwarder
 
 def parse_args():
     parser = argparse.ArgumentParser(description='A script to generate images using provided target and server information.')
-    parser.add_argument('-t', '--target', type=str,            required=True, help='The JSON file containing the information for the task.')
-    parser.add_argument('-c', '--config', type=str,            required=True, help='The JSON file defining the redis connection.')
-    parser.add_argument('-s', '--size',  type=int,             required=True, help='The number of images to generate total.')
-    parser.add_argument('-b', '--batch',  type=int,            default=2,     help='The number of images to generate per batch(limited by server max).')
-    parser.add_argument('-p', '--path',   type=str,            required=True, help='The base label to store the files.')
-    parser.add_argument('-x', '--srx',    type=str,                           help='Search and replace for axis X in the format: <original>,<alt1>,<alt2>')
-    parser.add_argument('-y', '--sry',    type=str,                           help='Search and replace for axis Y in the format: <original>,<alt1>,<alt2>')
-    parser.add_argument('-z', '--srz',    type=str,                           help='Search and replace for axis Z in the format: <original>,<alt1>,<alt2>')
+    parser.add_argument('-t', '--target',   type=str, required=True, help='The JSON file containing the information for the task.')
+    parser.add_argument('-c', '--config',   type=str, required=True, help='The JSON file defining the redis connection.')
+    parser.add_argument('-s', '--size',     type=int, required=True, help='The number of images to generate total.')
+    parser.add_argument('-b', '--batch',    type=int, default=2,     help='The number of images to generate per batch(limited by server max).')
+    parser.add_argument('-p', '--path',     type=str, required=True, help='The base label to store the files.')
+    parser.add_argument('-r', '--priority', type=int, default=0,     help='The priority of the job. Higher priority jobs are processed first.')
+    parser.add_argument('-x', '--srx',      type=str,                help='Search and replace for axis X in the format: <original>,<alt1>,<alt2>')
+    parser.add_argument('-y', '--sry',      type=str,                help='Search and replace for axis Y in the format: <original>,<alt1>,<alt2>')
+    parser.add_argument('-z', '--srz',      type=str,                help='Search and replace for axis Z in the format: <original>,<alt1>,<alt2>')
     return parser.parse_args()
 
 def combine_arrays(*args):
@@ -39,6 +40,7 @@ def main():
     output_path  = args.path
     batch_size   = args.batch
     config_file  = args.config
+    priority     = args.priority
 
     srx = args.srx.split(',') if args.srx else None
     sry = args.sry.split(',') if args.sry else None
@@ -74,7 +76,7 @@ def main():
 
             payload['prompt'] = prompt
             payload['batch_size'] = batch_size
-            redis_handler.send_job_to_ingress_queue(redis_connection, base_queue_name, payload, return_queue, [root_path,output_path,*change])
+            redis_handler.send_job_to_ingress_queue(redis_connection, base_queue_name, payload, return_queue, [root_path,output_path,*change], priority)
             print(".")
         print("Done sending jobs for loop: {}".format(i))
     print("Done generating images.")
