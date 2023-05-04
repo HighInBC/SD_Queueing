10bb18fbf339d1bf0ab590e89c5b93c9e2e701f3
HighInBC <HighInBC@gmail.com>
2023-05-04 15:56:05 -0700

    Okay actually read the docs lol

--- empty_queue.py
+++ empty_queue.py
@@ -7,14 +7,9 @@ from sdq.config_parser import ConfigParser
 def handle_job(job):
     print("Emptying queue...")
 
-def empty_queue(redis_connection, queue, batch_size=1000):
-    while True:
-        entries = redis_connection.lrange(queue, 0, batch_size - 1)
-        if not entries:
-            break
-        for entry in entries:
-            handle_job(entry)
-        redis_connection.ltrim(queue, batch_size, -1)
+def empty_queue(redis_connection, queue):
+    print("Emptying queue...")
+    redis_connection.delete(queue)
 
 def main():
     config = ConfigParser(config_file='config.json')
@@ -26,7 +21,6 @@ def main():
         sys.exit(1)
 
     queue = sys.argv[1]
-
     empty_queue(redis_connection, queue)
     
 if __name__ == "__main__":
