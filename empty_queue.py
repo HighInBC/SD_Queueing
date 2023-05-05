44790ce786ca513fe99380dff4ff130ef46ed8aa
HighInBC <HighInBC@gmail.com>
2023-05-04 19:11:28 -0700

    Simplify

--- empty_queue.py
+++ empty_queue.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 import sys
-from sdq.redis_handler import connect_to_redis, read_from_return_queue, InvalidInputException
+from sdq.redis_handler import connect_to_redis, InvalidInputException
 from sdq.config_parser import ConfigParser
-
-def handle_job(job):
-    print("Emptying queue...")
-
-def empty_queue(redis_connection, queue):
-    print("Emptying queue...")
-    redis_connection.delete(queue)
 
 def main():
     config = ConfigParser(config_file='config.json')
-    global counter
-    counter = {}
 
     try:
         redis_connection = connect_to_redis(config)
@@ -22,7 +13,8 @@ def main():
         sys.exit(1)
 
     queue = sys.argv[1]
-    empty_queue(redis_connection, queue)
+    print("Emptying queue...")
+    redis_connection.delete(queue)
     
 if __name__ == "__main__":
     main()
