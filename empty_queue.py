4c2cd6d2851c24d30bf4cf8d9852b3975a4e5b83
HighInBC <HighInBC@gmail.com>
2023-05-05 14:25:31 -0700

    Extend error handling

--- empty_queue.py
+++ empty_queue.py
@@ -6,15 +6,33 @@ from sdq.config_parser import ConfigParser
 def main():
     config = ConfigParser(config_file='config.json')
 
+    if len(sys.argv) != 2:
+        print("Usage: ./empty_redis_queue.py <queue>")
+        sys.exit(2)
+
+    queue = sys.argv[1]
+
+    if not queue:
+        print("Error: Queue name cannot be empty")
+        sys.exit(2)
+
     try:
         redis_connection = connect_to_redis(config)
     except InvalidInputException as e:
         print(f"Error: {e}")
+        sys.exit(2)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
         sys.exit(1)
 
-    queue = sys.argv[1]
     print("Emptying queue...")
-    redis_connection.delete(queue)
-    
+    try:
+        redis_connection.delete(queue)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    sys.exit(0)
+
 if __name__ == "__main__":
     main()
